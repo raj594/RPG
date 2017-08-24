28b21b483297ac7ba56a1df85040204c98798f14
raj594 <rajaynes@gmail.com>
2017-08-24 15:46:03 -0500

    create logic and styling for game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,95 +1,173 @@
-var targetNumber = 0;
-var wins = 0;
-var losses = 0;
-var counter = 0;
-
-//Print Original State
-$("#wins").text(wins);
-$("#losses").text(losses);
-$("#currentNumber").text(counter);
-
-
-// Set game state
-
-
-
-// Create number to guess from 19 - 120
-targetNumber = (Math.floor(Math.random() * 102) + 19);
-$("#number-to-guess").text(targetNumber);
-
-// Get 4 random numbers from 1 - 12 and set them in an array
-var one = (Math.floor(Math.random() * 12) + 1);
-var two = (Math.floor(Math.random() * 12) + 1);
-var three = (Math.floor(Math.random() * 12) + 1);
-var four = (Math.floor(Math.random() * 12) + 1);
-var numberOptions = [one, two, three, four];
-
-
-
-// Create a for loop to create crystals for every numberOption.
-for (var i = 0; i < numberOptions.length; i++) {
-
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-    imageCrystal.attr("number", i);
-
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
+var vince = {
+    name: "Vince Young",
+    health: 57,
+    attack: 6,
+    counterAttack: 9,
+    pic: "assets/images/vince.jpg"
 }
 
-// This time, our click event applies to every single crystal on the page. Not just one.
-$(".crystal-image").on("click", function() {
+var boz = {
+    name: "The Boz",
+    health: 56,
+    attack: 7,
+    counterAttack: 8,
+    pic: "assets/images/theboz.jpg"
+}
 
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+var crabtree = {
+    name: "Crabtree",
+    health: 55,
+    attack: 8,
+    counterAttack: 7,
+    pic: "assets/images/crabtree.jpg"
+}
 
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
+var jff = {
+    name: "Johnny Football",
+    health: 53,
+    attack: 9,
+    counterAttack: 6,
+    pic: "assets/images/manziel.jpg"
+}
 
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
-    $("#currentNumber").text(counter);
+var charSelected = false;
+var defenderSelected = false;
+var alive = false;
+var heroHealth = 0;
+var heroAttack = 0;
+var defenderHealth = 0;
+var defenderAttack = 0;
+var heroName = "";
+var cpuName = "";
+var defeated = 0;
+var damage = 0;
+var baseAttack = 0;
 
-    if (counter === targetNumber) {
-      wins++;
-      $("#wins").text(wins);
-      alert("You win!");
-      clearStatus();
-    } else if (counter >= targetNumber) {
-      losses++;
-      $("#losses").text(losses);
-      alert("You lose!!");
-      clearStatus();
+var chars = [vince, boz, crabtree, jff];
+
+for (var i = 0; i < chars.length; i++) {
+    var newChar = $("<div>");
+    newChar.addClass("character");
+    newChar.prepend("<img id=" + chars[i].name + " class=\"char\" src=" + chars[i].pic + ">");
+    newChar.prepend("<p>" + chars[i].name + "</p>");
+    newChar.append("<div class=\"health\">" + chars[i].health);
+    newChar.attr("health", chars[i].health);
+    newChar.attr("attack", chars[i].attack);
+    newChar.attr("counterattack", chars[i].counterAttack);
+    newChar.attr("player", chars[i].name);
+    $("#characters").append(newChar);
+}
+
+//Print Original State
+function originalState() {
+    $(".character").each(function(i) {
+        $(this).show("#characters");
+        $(this).appendTo("#characters");
+        $(this).find('div').html($(this).attr('health'));
+        $(this).removeClass('hero');
+
+    });
+
+    //Reset HTML
+    $("#yourhp").text("");
+    $("#cpuhp").text("");
+    $('.reset').remove();
+
+    //Reset flags and vars
+    charSelected = false;
+    defenderSelected = false;
+    alive = false;
+    heroName = "";
+    cpuName = "";
+    defeated = 0;
+
+
+};
+
+originalState();
+
+//Event handler for when a character is pressed.
+$(".character").on("click", function() {
+    //if a character hasnt been selected, we'll select one
+    if (charSelected === false) {
+        charSelected = true;
+        alive = true;
+        $(this).siblings('.character').appendTo('#enemies');
+        $(this).addClass('hero');
+        $(this).appendTo('#yourChar');
+        heroHealth = $(this).attr("health");
+        heroAttack = $(this).attr("attack");
+        baseAttack = $(this).attr("attack");
+        heroName = $(this).attr("player");
+
+    //if we have a character, if an enemy is pressed we will make it the defender
+    } else if (charSelected === true && defenderSelected === false && $(this).hasClass("hero") === false) {
+        defenderHealth = $(this).attr("health");
+        defenderAttack = $(this).attr("counterattack");
+        cpuName = $(this).attr("player");
+        $(this).appendTo('#defender');
+        defenderSelected = true;
     }
 
 });
 
-function clearStatus() {
-  counter = 0;
-  $("#currentNumber").text(counter);
-  targetNumber = (Math.floor(Math.random() * 102) + 19);
-  $("#number-to-guess").text(targetNumber);
-  one = (Math.floor(Math.random() * 12) + 1);
-  two = (Math.floor(Math.random() * 12) + 1);
-  three = (Math.floor(Math.random() * 12) + 1);
-  four = (Math.floor(Math.random() * 12) + 1);
-  numberOptions = [one, two, three, four];
-  $(".crystal-image").each(function(i, number){
-    $(this).attr('data-crystalvalue', numberOptions[i]);
-  });
-};
+//Event handler for when attack button is pressed
+$("#attack").on("click", function() {
+    if (charSelected === true && defenderSelected === true && alive === true) {
+        heroHealth -= defenderAttack;
+        defenderHealth -= heroAttack;
+        damage = heroAttack;
+        heroAttack = parseInt(heroAttack) + parseInt(baseAttack);
+
+        $(".character").each(function(i) {
+
+            if ($(this).attr('player') === heroName) {
+                $(this).find('div').html(heroHealth);
+            };
+
+            if ($(this).attr('player') === cpuName) {
+                $(this).find('div').html(defenderHealth);
+            };
+
+        });
+
+        if (heroHealth <= 0) {
+            $("#yourhp").text("you lose, loser.");
+            $("#cpuhp").text("");
+            alive = false;
+            $("#reset").append("<button class='reset'>reset</button>");
+
+        } else if (defenderHealth <= 0) {
+            defenderSelected = false;
+            defeated++;
+            $("#yourhp").text("You have defeated " + cpuName + ", you can choose to fight another enemy.");
+            $("#cpuhp").text("");
+
+            $(".character").each(function(i) {
+                if ($(this).attr('player') === cpuName) {
+                    $(this).hide();
+                };
+            });
+
+        } else {
+            $("#yourhp").text("You attacked " + cpuName + " for " + damage + " damage.");
+            $("#cpuhp").text(cpuName + " attacked you back for " + defenderAttack + " damage.");
+        }
+
+        if (defeated === 3) {
+            defeated = 0;
+            $("#reset").append("<button class='reset'>reset</button>");
+            $("#yourhp").text("You won Game Over!");
+            $("#cpuhp").text("");
+        };
+    } else if (charSelected === true && defenderSelected === false && alive === true) {
+        $("#yourhp").text("No enemy here.");
+        $("#cpuhp").text("");
+    }
+
+});
+
+//Event handler for when reset button is pressed
+$("#reset").on("click", function() {
+    originalState();
+});
